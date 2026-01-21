8c72d6b7a1fd350d7d18308d24929e042257c89e
slay <gragwitney@gmail.com>
2026-01-22 01:09:41 +0300

    everything seems to be ok but i forgot about .env file

--- run.py
+++ run.py
@@ -34,3 +34,5 @@ if __name__ == '__main__':
         print('Stop')
     except Exception as e:
         logging.critical("Критическая ошибка при запуске бота!", exc_info=True)  
+        
+# i wrote this text just for a push while testing ci/cd
